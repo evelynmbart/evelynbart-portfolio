4a27d83a1c40ff8b0887eb054a7dae7a53e19bcd
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-10-24 14:44:12 -0400

    implement hero jsx (basic html) layout

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,8 +2,41 @@ import "./Hero.css";
 
 export function Hero() {
   return (
-    <div>
-      <div className="hero">hero</div>
-    </div>
+    <section id="hero" className="hero-container">
+      <div className="hero-color-mode">
+        <img
+          className="avatar"
+          src="/src/assets/avatar.png"
+          alt="profile avatar of Evelyn Bart"
+        />
+        <img
+          className="colorMode"
+          src="/src/assets/moon.png"
+          alt="Color mode icon"
+        />
+      </div>
+      <div className="hero-info">
+        <h1>
+          Evelyn <br />
+          Bart
+        </h1>
+        <h2>Frontend Developer</h2>
+        <span>
+          <a href="https://github.com/evelynmbart" target="blank">
+            <img src="/src/assets/github-dark.png" alt="github logo" />
+          </a>
+          <a href="www.linkedin.com/in/evelyn-bart-a469a4288" target="blank">
+            <img src="/src/assets/linkedin-dark.png" alt="linkedin logo" />
+          </a>
+        </span>
+        <p>
+          With a passion for developing modern React web apps for commercial
+          businesses.
+        </p>
+        <a href="/src/assets/Evelyn Bart 2024 Resume copy.pdf" download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
   );
 }
