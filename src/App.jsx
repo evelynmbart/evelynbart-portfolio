97cd2a614a3a90f948ba19dc2477eec917b3eafd
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-10-24 14:03:54 -0400

    create blank slate for project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <a target="_blank" href="https://icons8.com/icon/447/linkedin">
-        LinkedIn
-      </a>{" "}
-      icon by{" "}
-      <a target="_blank" href="https://icons8.com">
-        Icons8
-      </a>
-    </>
-  );
+  return <>App</>;
 }
 
 export default App;
