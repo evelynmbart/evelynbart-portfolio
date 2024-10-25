ff619a58ab39212ba93fe148f8dae03cc56cb638
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-10-25 12:40:07 -0400

    create and import skills.jsx file

---
+++ src/components/Skills/Skill.jsx
@@ -0,0 +1,9 @@
+import "./Skills.css";
+
+export function Skills() {
+  return (
+    <section className="skills-container" id="skills">
+      <h1 className="sectionTitle">Skills</h1>
+    </section>
+  );
+}
