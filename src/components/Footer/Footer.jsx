2c12ef2cf44015a28049ce42b6eda0162d5d6494
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-10-25 15:53:16 -0400

    implement footer jsx and css

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+export function Footer() {
+  return (
+    <section className="footer-container" id="footer">
+      <p>
+        &copy; 2024 Evelyn Bart. <br />
+        All rights reserved.
+      </p>
+    </section>
+  );
+}
