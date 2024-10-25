8296a693b2fc27bfb818eb416860a8f2e1e58c72
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-10-25 12:46:26 -0400

    import checkmark images. begin skills.css and jsx

--- src/components/Skills/Skill.jsx
+++ src/components/Skills/Skill.jsx
@@ -1,9 +1,17 @@
 import "./Skills.css";
+import checkMarkDark from "../../assets/check-dark.png";
+import checkMarkLight from "../../assets/check-light.png";
 
 export function Skills() {
   return (
     <section className="skills-container" id="skills">
       <h1 className="sectionTitle">Skills</h1>
+      <div className="skills-list">
+        <span>
+          <img src={checkMarkDark} alt="Checkmark icon" />
+          <p>html</p>
+        </span>
+      </div>
     </section>
   );
 }
