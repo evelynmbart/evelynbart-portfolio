b264210d70b48796a7c2a75a15c90eb60cf2f451
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-10-25 13:29:21 -0400

    create contact jsx, css files and import into App.jsx

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,9 @@
+import "./Contact.css";
+
+export function Contact() {
+  return (
+    <section className="contact-container" id="contact">
+      <h1 className="sectionTitle">Contact</h1>
+    </section>
+  );
+}
