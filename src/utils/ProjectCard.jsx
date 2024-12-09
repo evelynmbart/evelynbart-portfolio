81db4f51ed00b643bc4d11ec844a3aef9061f981
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-12-09 13:42:57 -0500

    update project card css and linking href

--- src/utils/ProjectCard.jsx
+++ src/utils/ProjectCard.jsx
@@ -1,6 +1,6 @@
 export function ProjectCard({ project }) {
   return (
-    <a target="blank">
+    <a target="blank" href={project.link}>
       <img className="hover" src={project.src} alt={project.title} />
       <h3>{project.title}</h3>
       <p>{project.description}</p>
