6a2a5567d479ce40692dba885ae41b2676b72f6b
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-12-09 16:55:24 -0500

    add toggle to nav in mobile view

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,13 @@
+import moon from "../../assets/moon.png";
+import sun from "../../assets/sun.png";
+import { useTheme } from "../../utils/ThemeContext";
 import "./Navbar.css";
 
 export function Navbar() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === "light" ? sun : moon;
+
   return (
     <div className="navbar-container" id="navbar">
       <h3>Welcome to my Web Dev Porfolio!</h3>
@@ -8,6 +15,14 @@ export function Navbar() {
         <a href="#projects">Projects</a>
         <a href="#skills">Skills</a>
         <a href="#contact"> Contact</a>
+      </div>
+      <div className="nav-color-mode">
+        <img
+          className="nav-colorMode"
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
       </div>
     </div>
   );
