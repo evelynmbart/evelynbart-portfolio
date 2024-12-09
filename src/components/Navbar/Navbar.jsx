53eac2d191e5311bc3629eb271f9cb0e7e38118e
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-12-09 15:48:28 -0500

    add in menu icon from icons8 and toggle it using theme

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,13 @@
+import menuDark from "../../assets/menu-dark.png";
+import menuLight from "../../assets/menu-light.png";
+import { useTheme } from "../../utils/ThemeContext";
 import "./Navbar.css";
 
 export function Navbar() {
+  const { theme, toggleTheme } = useTheme();
+
+  const menuIcon = theme === "light" ? menuLight : menuDark;
+
   return (
     <div className="navbar-container" id="navbar">
       <h3>Welcome to my Web Dev Porfolio!</h3>
@@ -8,6 +15,9 @@ export function Navbar() {
         <a href="#projects">Projects</a>
         <a href="#skills">Skills</a>
         <a href="#contact">Contact</a>
+      </div>
+      <div className="menu">
+        <img src={menuIcon} />
       </div>
     </div>
   );
