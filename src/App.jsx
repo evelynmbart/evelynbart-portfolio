b619692a71fff1af524e44fffbe4e19f0500881e
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-12-18 14:04:33 -0500

    create and import now.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Contact } from "./components/Contact/Contact";
 import { Footer } from "./components/Footer/Footer";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
+import { Now } from "./components/Now/Now";
 import { Projects } from "./components/Projects/Projects";
 import { Skills } from "./components/Skills/Skill";
 
@@ -11,6 +12,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <Now />
       <Projects />
       <Skills />
       <Contact />
---
+++ src/components/Now/Now.jsx
@@ -0,0 +1,9 @@
+import "./Now.css";
+
+export function Now() {
+  return (
+    <section id="now" className="now">
+      <h1>now</h1>
+    </section>
+  );
+}
