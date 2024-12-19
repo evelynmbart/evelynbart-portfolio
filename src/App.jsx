85695d11e022c45c08ccf2910b9ce2af5726f50a
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-12-18 20:12:00 -0500

    update now section but comment it out and wait to flesh it out

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Contact } from "./components/Contact/Contact";
 import { Footer } from "./components/Footer/Footer";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
-import { Now } from "./components/Now/Now";
 import { Projects } from "./components/Projects/Projects";
 import { Skills } from "./components/Skills/Skill";
 
@@ -12,7 +11,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
-      <Now />
+      {/* <Now /> */}
       <Projects />
       <Skills />
       <Contact />
--- src/components/Now/Now.jsx
+++ src/components/Now/Now.jsx
@@ -3,7 +3,19 @@ import "./Now.css";
 export function Now() {
   return (
     <section id="now" className="now">
-      <h1>now</h1>
+      <h1 className="sectionTitle">Now</h1>
+      <div className="text-box">
+        {/* <p>
+          First off, a BIG welcome to my frontend developer portfolio! I'm
+          beyond happy to see you're here, so thank you! I hope you learn
+          everything you need to know and more about me as a Frontend Developer!
+        </p>
+        <br /> */}
+        <p>
+          I'm currently on the lookout for opportunities to prove my worth as a
+          developer.
+        </p>
+      </div>
     </section>
   );
 }
